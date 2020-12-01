10aa04d029415b04643dcc98c3a1a694072e50ff
Thomas Holmes <thomasholmes@outlook.com>
2020-12-01 21:38:16 +0000

    Added logging to file services.

--- src/main/java/com/example/entityapi/service/FileStorageService.java
+++ src/main/java/com/example/entityapi/service/FileStorageService.java
@@ -2,7 +2,7 @@ package com.example.entityapi.service;
 
 public interface FileStorageService {
 
-    Boolean uploadFile(String fileName, byte[] data);
+    void uploadFile(String fileName, byte[] data);
 
     byte[] downloadFile(String fileName);
 }
