91000622c44e1ca562bfdde711de30db8e8972f4
Thomas Holmes <thomasholmes@outlook.com>
2020-12-01 23:38:12 +0000

    Added home controller with build information.

---
+++ src/main/java/com/example/entityapi/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.entityapi.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HomeController {
+
+    @Value("${git.build.time}")
+    private String buildTime;
+    
+    @Value("${git.build.version}")
+    private String version;
+
+    @Value("${git.commit.id.abbrev}")
+    private String hash;
+
+    @GetMapping
+    public String home() {
+        return String.format("Entity API version %s. Build time %s. Hash: %s", version, buildTime, hash);
+    }
+}
