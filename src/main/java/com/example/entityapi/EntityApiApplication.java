b604289bbe9a5b9a76fe6f9deda8c892ece19394
Thomas Holmes <thomasholmes@outlook.com>
2020-12-01 21:14:50 +0000

    Added zalando exception handler.

--- src/main/java/com/example/entityapi/EntityApiApplication.java
+++ src/main/java/com/example/entityapi/EntityApiApplication.java
@@ -1,13 +1,28 @@
 package com.example.entityapi;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.zalando.problem.ProblemModule;
+import org.zalando.problem.violations.ConstraintViolationProblemModule;
 
 @SpringBootApplication
-public class EntityApiApplication
-{
-	public static void main(String[] args)
-	{
+@EnableAutoConfiguration(exclude = ErrorMvcAutoConfiguration.class)
+public class EntityApiApplication {
+	public static void main(String[] args) {
 		SpringApplication.run(EntityApiApplication.class, args);
+	}
+
+	/**
+	 * Creates object mapper with zalando problem modules.
+	 * @return
+	 */
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper().registerModules(new ProblemModule(), new ConstraintViolationProblemModule());
 	}
 }
