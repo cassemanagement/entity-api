8a530b6619597184ba520162f26eb3031f70e162
Thomas Holmes <thomasholmes@outlook.com>
2020-12-02 17:10:59 +0000

    Added date serialisation annotation.

--- src/main/java/com/example/entityapi/model/Entity.java
+++ src/main/java/com/example/entityapi/model/Entity.java
@@ -2,6 +2,7 @@ package com.example.entityapi.model;
 
 import com.example.entityapi.model.entity.attributes.Attribute;
 import com.example.entityapi.model.entity.attributes.Comment;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.microsoft.spring.data.gremlin.annotation.Vertex;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,6 +11,8 @@ import org.springframework.data.annotation.Id;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -40,5 +43,8 @@ public class Entity
 
 	private String createdBy;
 
-	private String createdDate;
+	// Date is used as only that is supported by Gremlin. JsonFormat is com.fasterxml.jackson (not org.apache.tinkerpop.shaded.jackson)
+	// so this annotation os only used at the user boundary.
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+	private Date createdDate;
 }
