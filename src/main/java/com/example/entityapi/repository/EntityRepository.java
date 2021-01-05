65a8c34cdcff49a4e0dde4c98198749dbfdf32e4
Jordan Cassem <jordancassem@gmail.com>
2021-01-05 12:20:47 +0000

    Add get by type function

--- src/main/java/com/example/entityapi/repository/EntityRepository.java
+++ src/main/java/com/example/entityapi/repository/EntityRepository.java
@@ -4,10 +4,14 @@ import com.example.entityapi.model.Entity;
 import com.microsoft.spring.data.gremlin.repository.GremlinRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 /**
  * Repository for entity vertices.
  */
 @Repository
 public interface EntityRepository extends GremlinRepository<Entity, String>
 {
+    // NB Gremlin currently doesn't support 'IgnoreCase'
+    Collection<Entity> findByType(String type);
 }
